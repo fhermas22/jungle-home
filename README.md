832f4b97649d8b3ca4271e834eed947517ae4430
Hermas Francisco <franciscohermas@gmail.com>
2026-01-15 11:12:53 +0100

    Discover Components in JSX

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # "La Maison Jungle" (A Jungle Home)
 
 Practical project with React carried out as part of the "Getting Started with React" (Débutez avec React) course by OpenClassrooms.
+
+# Course Resources 
+
+- Course OpenClassrooms Link : https://openclassrooms.com/fr/courses/7008001-debutez-avec-react/
+- Course GitHub Repository : https://github.com/fhermas22/start-with-react
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,29 @@
 function App() {
   return (
     <>
+      <Banner />
     </>
+  )
+}
+
+function Banner() {
+  return (
+    <div>
+      <Header />
+      <Description />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <h1>La maison jungle</h1>
+  )
+}
+
+function Description() {
+  return (
+    <p>Ici achetez toutes les plantes dont vous avez toujours rêvé 🌵🌱🎍</p>
   )
 }
 
