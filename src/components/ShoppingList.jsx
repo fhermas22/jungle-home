fef827c6db5bd69e084cdcd268095142503a3d55
Hermas Francisco <franciscohermas@gmail.com>
2026-01-16 15:54:31 +0100

    Complete Pratice P2C4 : Create new component PlantItem and apply props to it

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,15 +1,15 @@
-import {plantList} from '../datas/plantList'
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
-import CareScale from './CareScale'
-
-const categories = plantList.reduce(
-	(acc, plant) =>
-		acc.includes(plant.category) ? acc : acc.concat(plant.category),
-	[]
-)
 
 function ShoppingList() {
-    return (
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
 		<div>
 			<ul>
 				{categories.map((cat) => (
@@ -17,17 +17,18 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-						<CareScale careType='water' scaleValue={plant.water} />
-						<CareScale careType='light' scaleValue={plant.light} />
-					</li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
-    )
+	)
 }
 
 export default ShoppingList
